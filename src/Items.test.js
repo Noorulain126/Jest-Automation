56984ff172ccb959bbfd87c94f31ef66920f617b
Noorulain126 <Noorulain126@gmail.com>
2023-12-20 12:12:59 +0500

    initial workflow for tests

--- src/Items.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Items from './Items'; // Fix the typo by adding a closing single quote here
-
-test('renders correctly when there are no items', () => {
-  const tree = renderer.create(<Items />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
