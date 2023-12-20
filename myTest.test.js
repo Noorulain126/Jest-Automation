6e98f635d37098e17ee2718c565fda99b0c06a3f
Noorulain126 <Noorulain126@gmail.com>
2023-12-20 17:58:53 +0500

    Creating first PR

--- myTest.test.js
+++ myTest.test.js
@@ -1,5 +1,6 @@
-const myFunction = require('./myFunction');
+// import {toMatchSnapshot} from 'jest';
+// const myFunction = require('././myFunction');
 
-test('myFunction returns the expected result', () => {
-  expect(myFunction()).toMatchSnapshot();
-});
+// test('myFunction returns the expected result', () => {
+//   expect(myFunction()).toMatchSnapshot();
+// });
